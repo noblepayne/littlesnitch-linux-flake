d337d3f5b6f992bc70a273011ed1d8ba4c256580
noblepayne <33278192+noblepayne@users.noreply.github.com>
2026-07-13 14:09:38 +0000

    littlesnitch: 1.0.9-1 -> 1.1.0-1

--- pkgs/sources.nix
+++ pkgs/sources.nix
@@ -1,19 +1,19 @@
 {
-  version = "1.0.9-1";
+  version = "1.1.0-1";
   aarch64-linux = {
-    url = "https://obdev.at/downloads/littlesnitch-linux/littlesnitch-1.0.9-1-aarch64.pkg.tar.zst";
-    hash = "sha256-SNg9RI/Gx5S5+tULrU6uVfBhs9WQbOzQ+xTwP2ovgLM=";
+    url = "https://obdev.at/downloads/littlesnitch-linux/littlesnitch-1.1.0-1-aarch64.pkg.tar.zst";
+    hash = "sha256-VQKb+Q/XoTz4bcywHVH1VDzMdotD8X9QaNW4pzCJ5HY=";
   };
   ppc64le-linux = {
-    url = "https://obdev.at/downloads/littlesnitch-linux/littlesnitch-1.0.9-1-ppc64le.pkg.tar.zst";
-    hash = "sha256-IDh7zserE9Mxk8iEXmJyyTkUcb36eBPm9+ioceBtfdc=";
+    url = "https://obdev.at/downloads/littlesnitch-linux/littlesnitch-1.1.0-1-ppc64le.pkg.tar.zst";
+    hash = "sha256-DMWOAiwuHOdMEyd8JEg8IahevQVOAfwP+f/7TPYI53A=";
   };
   riscv64-linux = {
-    url = "https://obdev.at/downloads/littlesnitch-linux/littlesnitch-1.0.9-1-riscv64.pkg.tar.zst";
-    hash = "sha256-JHhrRJDl3Zk2h3x/ul1zYUMeLsg9Hz/TYZb9+qr/Qnw=";
+    url = "https://obdev.at/downloads/littlesnitch-linux/littlesnitch-1.1.0-1-riscv64.pkg.tar.zst";
+    hash = "sha256-engxPWMOpEkK7emnyZh7zH8FhkMp5HfDCsz/DYushyA=";
   };
   x86_64-linux = {
-    url = "https://obdev.at/downloads/littlesnitch-linux/littlesnitch-1.0.9-1-x86_64.pkg.tar.zst";
-    hash = "sha256-4y3vs376dAza9X4FMEqkxCC5sPC2RTCY8XlGOfjtB08=";
+    url = "https://obdev.at/downloads/littlesnitch-linux/littlesnitch-1.1.0-1-x86_64.pkg.tar.zst";
+    hash = "sha256-YY9B6Kt/ej/1tQ1TJAvU7tPBav8/2rTrwKMIzfF7IIg=";
   };
 }
